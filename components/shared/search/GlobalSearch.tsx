90b6cced61d78ebdb16685f119d77e70ed399a4c
Yogesh Bisht <reinzire@gmail.com>
2023-11-03 02:55:17 +0530

    left sidebar layout component

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
@@ -18,6 +20,7 @@ const GlobalSearch = () => {
           placeholder="Search..."
           value=""
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          onChange={() => {}}
         />
       </div>
     </div>
