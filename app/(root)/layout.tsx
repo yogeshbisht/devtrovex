f872637f27a6ca07fa28d39a8c9b3e23694c1d4d
Yogesh Bisht <reinzire@gmail.com>
2023-11-04 12:33:58 +0530

    right side bar component layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
+
+import Navbar from "@/components/shared/navbar/Navbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +13,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full">{children}</div>
         </section>
-        RightSidebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
