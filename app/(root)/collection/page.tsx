59ccd33e5fb1c65665271512ab634d2b0024ee64
Yogesh Bisht <reinzire@gmail.com>
2023-11-20 20:53:15 +0530

    collection page layout and getting saved questions

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,59 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+
+export default async function CollectionPage() {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({ clerkId: userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="right"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There are no saved questions to show"
+            description="You can save questions by clicking on the bookmark icon on the question card. You can also ask a question. Go ahead"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+}
