68fd822b46c27ef1e1b8d6fe2c71aed6f330ac70
Yogesh Bisht <reinzire@gmail.com>
2023-11-23 17:01:02 +0530

    show top questions and most popular tags on right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,27 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    { _id: "1", title: "How do I use express as a custom server in Next.js?" },
-    { _id: "2", title: "Cascading Deletes in SQLAlchemy" },
-    { _id: "3", title: "How to Perfectly Center a Div with Tailwind CSS" },
-    {
-      _id: "4",
-      title:
-        "Best practices for data fetching in a Next.js application with Sever-Side Rendering (SSR)?",
-    },
-    { _id: "5", title: "Redux Toolkit Not Updating State as Expected" },
-  ];
-
-  const popularTags = [
-    { _id: "1", name: "javascript", totalQuestions: 5 },
-    { _id: "2", name: "react", totalQuestions: 1 },
-    { _id: "3", name: "next.js", totalQuestions: 4 },
-    { _id: "4", name: "vue", totalQuestions: 3 },
-    { _id: "5", name: "redux", totalQuestions: 2 },
-  ];
+const RightSidebar = async () => {
+  const { hotQuestions } = await getHotQuestions();
+  const { popularTags } = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
@@ -31,7 +16,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -57,7 +42,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
