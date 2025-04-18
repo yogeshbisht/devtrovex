c4321edf0ef7595fd7a9c802483bc93a0f630e35
Yogesh Bisht <reinzire@gmail.com>
2025-04-18 17:50:29 +0530

    refactor eslint config and upgrade libraries

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { ReloadIcon } from "@radix-ui/react-icons";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import GlobalFilters from "./GlobalFilters";
 import { globalSearch } from "@/lib/actions/general.action";
-import { Tag } from "lucide-react";
+import { Loader2, Tag } from "lucide-react";
 
 const GlobalResult = () => {
   const searchParams = useSearchParams();
@@ -15,7 +14,7 @@ const GlobalResult = () => {
   const [result, setResult] = useState([
     { type: "question", id: 1, title: "Next.js question" },
     { type: "tag", id: 1, title: "Nextjs" },
-    { type: "user", id: 1, title: "jsm" },
+    { type: "user", id: 1, title: "jsm" }
   ]);
 
   const global = searchParams.get("global");
@@ -61,7 +60,7 @@ const GlobalResult = () => {
   return (
     <div className="absolute top-full z-10 mt-3 w-full bg-light-800 py-5 shadow-sm dark:bg-dark-400">
       <GlobalFilters />
-      <div className="my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50" />
+      <div className="my-5 h-px bg-light-700/50 dark:bg-dark-500/50" />
 
       <div className="space-y-5">
         <p className="text-dark400_light900 paragraph-semibold px-5">
@@ -70,7 +69,7 @@ const GlobalResult = () => {
 
         {isLoading ? (
           <div className="flex-center flex-col px-5">
-            <ReloadIcon className="my-2 h-10 w-10 animate-spin text-primary-500" />
+            <Loader2 className="my-2 size-10 animate-spin text-primary-500" />
             <p className="text-dark200_light800 body-regular">
               Browsing the entire database
             </p>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,155 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import qs from "query-string";
+import { BadgeCounts } from "@/types";
+import { BADGE_CRITERIA } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function getPluralString(num: number, str: string) {
+  return num > 1 ? `${str}s` : str;
+}
+
+export const getTimestamp = (createdAt: string | Date) => {
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const week = 7 * day;
+  const month = 30 * day;
+  const year = 365 * month;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const now = new Date();
+  const createdDate = new Date(createdAt);
+  const createdDay = createdDate.getDate();
+  const createdMonth = createdDate.getMonth();
+  const createdYear = createdDate.getFullYear();
+  const timeDifferent = now.getTime() - createdDate.getTime();
+
+  if (timeDifferent < minute) {
+    return "less a minute";
+  } else if (timeDifferent < hour) {
+    const minutes = Math.round(timeDifferent / minute);
+    return `${minutes} ${getPluralString(minutes, "minute")} ago`;
+  } else if (timeDifferent > hour && timeDifferent < day) {
+    const hours = Math.round(timeDifferent / hour);
+    return `${hours} ${getPluralString(hours, "hour")} ago`;
+  } else if (timeDifferent > day && timeDifferent < week) {
+    const days = Math.round(timeDifferent / day);
+    return `${days} ${getPluralString(days, "day")} ago`;
+  } else if (timeDifferent > week && timeDifferent < month) {
+    const weeks = Math.round(timeDifferent / week);
+    return `${weeks} ${getPluralString(weeks, "week")} ago`;
+  } else if (timeDifferent > month && timeDifferent < year) {
+    return `${months[createdMonth]} ${createdDay}`;
+  } else {
+    return `${createdDay} ${months[createdMonth]} ${createdYear}`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (!num) return "0";
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
+
+export const getJoinedDate = (date: Date): string => {
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+  return `${month} ${year}`;
+};
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string | null;
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl
+    },
+    { skipNull: true }
+  );
+};
+
+interface RemoveUrlQueryParams {
+  params: string;
+  keysToRemove: string[];
+}
+
+export const removeKeysFromQuery = ({
+  params,
+  keysToRemove
+}: RemoveUrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl
+    },
+    { skipNull: true }
+  );
+};
+
+interface BadgeParam {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA;
+    count: number;
+  }[];
+}
+
+export const assignBadges = ({ criteria }: BadgeParam) => {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0
+  };
+
+  criteria.forEach((item) => {
+    const { type, count } = item;
+    const badgeLevels: any = BADGE_CRITERIA[type];
+
+    Object.keys(badgeLevels).forEach((level: any) => {
+      if (count >= badgeLevels[level]) {
+        badgeCounts[level as keyof BadgeCounts] += 1;
+      }
+    });
+  });
+
+  return badgeCounts;
+};
